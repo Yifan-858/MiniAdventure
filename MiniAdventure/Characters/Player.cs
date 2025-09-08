7e80126b527a080a369cb4b714e3379479f26a6a
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2025-09-08 11:28:55 +0200

    initial commit, add Player

---
+++ MiniAdventure/Characters/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAdventure.Characters
+{
+    public class Player
+    {
+        public string Name{ get; set; }
+        public string HeroType{ get; set; }
+        public int HP { get; set; }
+        public int Damage { get; set; }
+        public int Gold = 0;
+
+        public Player(string name, string heroType)
+        {
+            //Throw an error if user enter an empty name.
+            if(string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Name can not be empty");
+            }
+
+            Name = name;
+            HeroType = heroType;
+
+            switch (HeroType)
+            {
+                case "Warrior":
+                    HP = 20;
+                    Damage = 5;
+                    break;
+
+                case "Mage":
+                    HP = 15;
+                    Damage = 10;
+                    break;
+
+                case "Rouge":
+                    HP = 18;
+                    Damage = 8;
+                    break;
+            }
+        }
+    }
+}
